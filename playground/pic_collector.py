41b5c5539eec5cd9f7476bb2a772e6fb37bdf89f
Sylvain Roy <sylvain.roy@m4x.org>
2019-09-01 14:53:03 +0200

    New script to collect images.

---
+++ playground/pic_collector.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Small script to take pictures on a regular basis.
+"""
+
+import asyncio
+
+from picamera import PiCamera
+from time import sleep
+from os.path import join
+
+
+camera = PiCamera()
+camera.rotation = 180
+
+FOLDER = "/home/pi/Pictures"
+
+async def take_pic():
+
+    while True:
+        for i in range(5):
+
+            time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            fname = f"img_{i}_{time}.jpg"
+            fpath = join(FOLDER, fname)
+
+            camera.start_preview()
+            #sleep(5)
+            asyncio.sleep(5)
+            camera.capture(fpath)
+            camera.stop_preview()
+
+        asyncio.sleep(5*60)
+
+loop = asyncio.get_event_loop()
+loop.run(take_pic())
